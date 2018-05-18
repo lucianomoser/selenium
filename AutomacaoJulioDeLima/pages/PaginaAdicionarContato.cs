5e3c48547d79d0f9b914487c1e04e054da898105
luciano moser <lucianomoser01@gmail.com>
2018-05-18 18:05:55 -0300

    Secao 4, Aula 274

--- AutomacaoJulioDeLima/pages/PaginaAdicionarContato.cs
+++ AutomacaoJulioDeLima/pages/PaginaAdicionarContato.cs
@@ -8,5 +8,30 @@ namespace AutomacaoJulioDeLima
         {
             this.navegador = navegador;
         }
+
+
+
+        public PaginaAdicionarContato InformarTipoContato(string tipo)
+        {
+            
+            return this;
+        }
+
+        public PaginaAdicionarContato digtarContato(string contato)
+        {
+            return this;
+        }
+
+        public PaginaMe ClicarSalvar()
+        {
+            return new PaginaMe(navegador);
+        }
+
+        public PaginaMe AdicionarContato(string tipo, string contato)
+        {
+            InformarTipoContato("phone");
+            digtarContato("4755555555");
+            return new PaginaMe(navegador);
+        }
     }
 }
